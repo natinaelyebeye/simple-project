0c8ad0dc7ff69efee18e98fffeec36ff264d5adb
natinaelyebeye <125657124+natinaelyebeye@users.noreply.github.com>
2023-02-18 04:48:04 -0800

    web-pro
    its-work

---
+++ eloginwel.php
@@ -0,0 +1,283 @@
+<?php 
+	$id = (isset($_GET['id']) ? $_GET['id'] : '');
+	require_once ('process/dbh.php');
+	 $sql1 = "SELECT * FROM `employee` where id = '$id'";
+	 $result1 = mysqli_query($conn, $sql1);
+	 $employeen = mysqli_fetch_array($result1);
+	 $empName = ($employeen['firstName']);
+
+	$sql = "SELECT id, firstName, lastName,  points FROM employee, rank WHERE rank.eid = employee.id order by rank.points desc";
+	$sql1 = "SELECT `pname`, `duedate` FROM `project` WHERE eid = $id and status = 'Due'";
+
+	$sql2 = "Select * From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
+
+	$sql3 = "SELECT * FROM `salary` WHERE id = $id";
+
+//echo "$sql";
+$result = mysqli_query($conn, $sql);
+$result1 = mysqli_query($conn, $sql1);
+$result2 = mysqli_query($conn, $sql2);
+$result3 = mysqli_query($conn, $sql3);
+?>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Admin page</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="assets/vendor1/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor1/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor1/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor1/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor1/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="assets/css/css1/main.css" rel="stylesheet">
+  <link href="assets/css/styleemplogin.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <section id="topbar" class="topbar d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <a href="index.html" class="logo d-flex align-items-center">
+         <img src="assets/img/logo.jpg" alt="rounded-circle" width="50px" height="50px">
+      </a>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href=""> S/N/N/P/R/W/ZONE SCINCE AND IT DEPARTMENT</a>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+            <a href="#">login</a>
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section><!-- End Top Bar -->
+
+  <header id="header" class="header d-flex align-items-center">
+
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <nav id="navbar" class="navbar">
+		<nav>
+			<h1>Wolaita Zone Science and Information Technology!!!.</h1>
+			<ul id="navli">
+				<li><a class="homered" href="eloginwel.php?id=<?php echo $id?>"">HOME</a></li>
+				<li><a class="homeblack" href="myprofile.php?id=<?php echo $id?>"">My Profile</a></li>
+				<li><a class="homeblack" href="empproject.php?id=<?php echo $id?>"">My Projects</a></li>
+				<li><a class="homeblack" href="applyleave.php?id=<?php echo $id?>"">Apply Leave</a></li>
+				<li><a class="homeblack" href="elogin.html">Log Out</a></li>
+			</ul>
+		</nav>
+	 <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
+	 
+	<div class="divider"></div>
+	<div id="divimg">
+	<div>
+		<!-- <h2>Welcome <?php echo "$empName"; ?> </h2> -->
+
+		    	<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Empolyee Leaderboard </h2>
+    	<table>
+
+			<tr bgcolor="#000">
+				<th align = "center">Seq.</th>
+				<th align = "center">Emp. ID</th>
+				<th align = "center">Name</th>
+				<th align = "center">Points</th>
+				
+
+			</tr>
+
+			
+
+			<?php
+				$seq = 1;
+				while ($employee = mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+					echo "<td>".$seq."</td>";
+					echo "<td>".$employee['id']."</td>";
+					
+					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
+					
+					echo "<td>".$employee['points']."</td>";
+					
+					$seq+=1;
+				}
+
+
+			?>
+
+		</table>
+   
+    	<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Due Projects</h2>
+    	
+
+    	<table>
+
+			<tr>
+				<th align = "center">Project Name</th>
+				<th align = "center">Due Date</th>
+				
+			</tr>
+
+			
+
+			<?php
+				while ($employee1 = mysqli_fetch_assoc($result1)) {
+					echo "<tr>";
+					
+					echo "<td>".$employee1['pname']."</td>";
+					
+					echo "<td>".$employee1['duedate']."</td>";
+
+				}
+
+
+			?>
+
+		</table>
+
+
+
+		<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Salary Status</h2>
+    	
+
+    	<table>
+
+			<tr>
+				
+				<th align = "center">Base Salary</th>
+				<th align = "center">Bonus</th>
+				<th align = "center">Total Salary</th>
+				
+			</tr>
+
+			
+
+			<?php
+				while ($employee = mysqli_fetch_assoc($result3)) {
+					
+
+					echo "<tr>";
+					
+					
+					echo "<td>".$employee['base']."</td>";
+					echo "<td>".$employee['bonus']." %</td>";
+					echo "<td>".$employee['total']."</td>";
+					
+				}
+
+
+				
+
+
+			?>
+
+		</table>
+
+
+
+
+
+
+
+
+
+
+		<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Leave Satus</h2>
+    	
+
+    	<table>
+
+			<tr>
+				
+				<th align = "center">Start Date</th>
+				<th align = "center">End Date</th>
+				<th align = "center">Total Days</th>
+				<th align = "center">Reason</th>
+				<th align = "center">Status</th>
+			</tr>
+
+			
+
+			<?php
+				while ($employee = mysqli_fetch_assoc($result2)) {
+					$date1 = new DateTime($employee['start']);
+					$date2 = new DateTime($employee['end']);
+					$interval = $date1->diff($date2);
+					$interval = $date1->diff($date2);
+
+					echo "<tr>";
+					
+					
+					echo "<td>".$employee['start']."</td>";
+					echo "<td>".$employee['end']."</td>";
+					echo "<td>".$interval->days."</td>";
+					echo "<td>".$employee['reason']."</td>";
+					echo "<td>".$employee['status']."</td>";
+					
+				}
+
+
+				
+
+
+			?>
+
+		</table>
+
+
+
+
+   
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+
+
+
+	</div>
+
+
+		</h2>
+
+
+		
+		
+	</div>
+</body>
+</html>
